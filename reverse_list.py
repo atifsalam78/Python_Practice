656922be97164efaaac3af374e5f4728fc4e5975
atifsalam78 <atif.salam@gmail.com>
2022-09-13 23:52:37 +0500

    Faulty calculator

--- reverse_list.py
+++
@@ -1,9 +0,0 @@
-# list1 = [10, 20, 30, 40, 50]
-# list1.reverse()
-# for item in list1:
-#     print(item)
-
-list1 = [10, 20, 30, 40, 50]
-list2 = reversed(list1)
-for item in list2:
-    print(item)
