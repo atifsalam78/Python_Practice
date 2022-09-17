74b77cbec02c3518114ff540b6222a3b9b693481
atifsalam78 <atif.salam@gmail.com>
2022-09-18 00:53:20 +0500

    some print messages modification

--- healthy_programmer.py
+++ healthy_programmer.py
@@ -23,28 +23,27 @@ while True:
 def activity():
     pkt_khi = pytz.timezone("Asia/Karachi")
     fmt = "%d-%m-%Y %H:%M:%S %Z%z"
-    now = datetime.now(pkt_khi)
+    act_now = datetime.now(pkt_khi)
     with open("activity.txt", "a") as log_file:
-        log_file.write(f"{user_name} {msg_print} on {now.strftime(fmt)} \n")
-        log_file.write("*************************************************\n")
+        log_file.write(f"{user_name} {msg_print} on {act_now.strftime(fmt)} \n")
 
 
-def sound_activity(act_sound):
-    import time
+def sound_activity(sound_file):
+    import os
+    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
     from pygame import mixer
     mixer.init()
-    mixer.music.load(act_sound)
+    mixer.music.load(sound_file)
     mixer.music.play()
-    while mixer.music.get_busy():  # wait for music to finish playing
+    while mixer.music.get_busy():
         time.sleep(1)
 
 
 def user_input():
     import string
-    global msg_print
     msg = ""
     while msg.strip(f"{string.punctuation} {string.digits}") != "done":
-        msg = input("""Write "done" :""")
+        msg = input(f"""Write "done" if you had :""")
 
 
 while True:
@@ -54,12 +53,12 @@ while True:
     time.sleep(1)
 
     if now.minute == alarm_set_water and now.second < 1:
-        print("\nWater Time \n")
+        print("\nWater Time")
         act_sound = "water.mp3"
         sound_activity(act_sound)
         user_input()
-        msg_print = "Drunk Water"
-        print(msg_print)
+        msg_print = "drunk water"
+        print(f"Great you {msg_print}")
         activity()
 
     elif now.minute == alarm_set_walk and now.second < 1:
@@ -67,8 +66,8 @@ while True:
         act_sound = "physical.mp3"
         sound_activity(act_sound)
         user_input()
-        msg_print = "Walked"
-        print(msg_print)
+        msg_print = "walked"
+        print(f"Great you {msg_print}")
         activity()
 
     elif now.minute == alarm_set_eye and now.second < 1:
@@ -76,16 +75,10 @@ while True:
         act_sound = "eyes.mp3"
         sound_activity(act_sound)
         user_input()
-        msg_print = "Relaxed Eyes"
-        print(msg_print)
+        msg_print = "eyes are relaxed"
+        print(f"Great your {msg_print}")
         activity()
 
-    if now.hour == 17 and now.minute == 30:
+    if now.hour == 17:
         print("Thanks for your contribution")
-    with open("activity.txt") as log_file:
-        for lines in log_file.readlines():
-            print(lines, end="")
-    break
-
-
-
+        break
