e1230b7999633dc09b138756babb98f238995ac2
atifsalam78 <atif.salam@gmail.com>
2022-10-10 21:29:47 +0500

    Combine project for reuest and pickle module

---
+++ iris_request_pickle.py
@@ -0,0 +1,20 @@
+import requests
+import pickle
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+re = requests.get(url=url)
+with open("iris_data.txt","wb") as pfd:
+    for ir_lines in re.iter_content(chunk_size=128):
+        pfd.write(ir_lines)
+
+with open("iris_data.txt") as pfd:
+    data_set = [lines.split() for lines in pfd.readlines()]
+    my_file = "iris_data_pickle.pkl"
+    with open (my_file, "wb") as file_object:
+        pickle.dump(data_set, file_object)
+
+my_file = "iris_data_pickle.pkl"
+file_object = open(my_file,"rb")
+my_data = pickle.load(file_object)
+print(my_data)
+print(type(my_data))
