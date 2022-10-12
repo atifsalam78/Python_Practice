ee8b4390e69c7d09224ef63859c19345e5f5ec3c
atifsalam78 <atif.salam@gmail.com>
2022-10-12 20:48:05 +0500

    Add direct method without using text file

--- iris_request_pickle.py
+++ iris_request_pickle.py
@@ -1,6 +1,8 @@
 import requests
 import pickle
 
+"""" Indirect method reading from ".txt file """
+
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 re = requests.get(url=url)
 with open("iris_data.txt","wb") as pfd:
@@ -16,5 +18,31 @@ with open("iris_data.txt") as pfd:
 my_file = "iris_data_pickle.pkl"
 with open (my_file,"rb") as file_object:
     my_data = pickle.load(file_object)
-    for list_lines in my_data:
-        print(list_lines)
+    # print(my_data)
+    md = [list_lines for list_lines in my_data if len(list_lines) != 0]
+    print(md)
+
+
+""" 
+Direct method without reading from ".txt" file
+
+"""
+
+# data = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data").text
+
+""" 
+print(data)
+l1 = data.split("\n") # not in use Just for understanding
+print(l1)
+"""
+# l2 = [item.split() for item in data.split("\n") if len(item)!=0]
+
+# print(l2)
+
+# with open("my_iris_pickle.pkl", "wb") as f:
+#     pickle.dump(l2, f)
+
+
+# with open("my_iris_pickle.pkl",'rb') as f:
+#     print(pickle.load(f))
+
