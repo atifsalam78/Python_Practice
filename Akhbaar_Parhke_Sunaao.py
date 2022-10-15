5043f24f3b098ce59c834ef5f114bcfc8a55af3b
atifsalam78 <atif.salam@gmail.com>
2022-10-15 23:00:26 +0500

    update to speak function

--- Akhbaar_Parhke_Sunaao.py
+++ Akhbaar_Parhke_Sunaao.py
@@ -1,20 +1,23 @@
-# Akhbaar Parhke Sunaao
 import requests
 import json
 
+
 def speak(str):
+    """Taking string as an argument and convert into audio"""
     from win32com.client import Dispatch
     speak = Dispatch("SAPI.Spvoice")
     speak.Speak(str)
 
+
 if __name__ == '__main__':
+
     print("News for today... Let's begin")
     url = "https://newsapi.org/v2/top-headlines?country=gb&apiKey=8e9e0b4fc17f49eeb1bc790352ccf965"
     news_pull = requests.get(url).text
     news_dict = json.loads(news_pull)
-    # print(news_dict["articles"])
-    arts = news_dict["articles"]        
+    arts = news_dict["articles"]
     for article in arts:
         print(article["title"])
-        print("Moving on to the next news.....")
-    print("Thanks for Reading")
+        speak(article["title"])
+        speak("Moving on to the next news.....")
+    speak("Thanks for Listening")
