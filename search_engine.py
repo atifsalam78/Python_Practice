65d1df414ab20ce2335977068718f35b8b085644
atifsalam78 <atif.salam@gmail.com>
2022-10-27 20:00:12 +0500

    Search word in python list

--- search_engine.py
+++ search_engine.py
@@ -1,25 +1,3 @@
-"""
-The task you have to perform is “Search Engine”. This task consists of a total of 20 points to evaluate your performance.
-
-Problem Statement:-
-You are given few sentences as a list (Python list of sentences). Take a query string as an input from the user. You have to pull out the sentences matching this query inputted by the user in decreasing order of relevance after converting every word in the query and the sentence to lowercase. The most relevant sentence is the one with the maximum number of matching words with the query.
-
-Sentences = [“Python is cool”, “python is good”, “python is not python snake”]
-
-Input:
-Please input your query string
-
-“Python is”
-
-Output:
-3 results found:
-
-python is not python snake
-python is good
-Python is cool
-Solving challenges will make you a great problem solver and will improve your coding skills.
-"""
-
 # First Method, TIme: 64.2 us
 # sentences = ["Python is cool", "python is good", "python is not python snake"]
 # search = input("Enter to search: ")
