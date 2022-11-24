36f18b7f514a0e76b07d44c8ab58b46b4cf9f0b0
atifsalam78 <atif.salam@gmail.com>
2022-11-24 21:17:33 +0500

    Fuzz string matchnig from partial ratio from 2 CSV files

--- stringMatching.py
+++ stringMatching.py
@@ -1,31 +1,14 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-with open ("cities.csv", "r") as f:
-    city = f.read().split("\n")
+with open ("cities.csv", "r") as f1, open ("citiesWrong.csv", "r") as f2:
+    city1 = f1.read().split("\n")
+    city2 = f2.read().split("\n")
 
-def getMatches(query, choices):
-    results = process.extractBests(query, choices)
-    print(results)
+def fuzMatches(file2, file1):
+    for wrongString, correctString in zip(file2, file1):
+        results = fuzz.partial_ratio(wrongString.lower(), correctString.lower())
+        print(f"{wrongString}, Fuzz Partial Ratio: {results}% from {correctString}")
 
-if __name__ =="__main__":
-    getMatches("Jhandi Chountra", city)
-
-    # x = fuzz.ratio("Catherine M Gitau","Catherine Gitau")
-    # print(f"Fuzzy Ratio {x}")
-    # y = fuzz.partial_ratio("Catherine M. Gitau","Catherine Gitau")
-    # print(f"Fuzzy Partial Ratio {y}")
-
-    # z = fuzz.token_sort_ratio("Catherine Gitau M.", "Gitau Catherine")
-    # print(f"Fuzzy Token Sort Ratio {z}")
-
-    # a = fuzz.token_set_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
-    # print(f"Fuzzy Token Set Ratio {a}")
-
-    # choices = ["Atlanta Falcons", "New York Jets", "New York Giants", "Dallas Cowboys"]
-    # b = process.extract("new york jets", choices, limit=2)
-    # print(f"Process Extract: {b}")
-
-    # c = process.extractOne("cowboys", choices)
-    # print(f"Process Extract One: {c}")
-
+if __name__ =="__main__":    
+    fuzMatches(city2, city1)
