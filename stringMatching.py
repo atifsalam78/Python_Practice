afd997399e37dff4f7b972c87756d22caf48c714
atifsalam78 <atif.salam@gmail.com>
2022-11-24 19:02:34 +0500

    Fuzzy string searching from csv

--- stringMatching.py
+++ stringMatching.py
@@ -1,23 +1,31 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-x = fuzz.ratio("Catherine M Gitau","Catherine Gitau")
-print(f"Fuzzy Ratio {x}")
-y = fuzz.partial_ratio("Catherine M. Gitau","Catherine Gitau")
-print(f"Fuzzy Partial Ratio {y}")
+with open ("cities.csv", "r") as f:
+    city = f.read().split("\n")
 
-z = fuzz.token_sort_ratio("Catherine Gitau M.", "Gitau Catherine")
-print(f"Fuzzy Token Sort Ratio {z}")
+def getMatches(query, choices):
+    results = process.extractBests(query, choices)
+    print(results)
 
-a = fuzz.token_set_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
-print(f"Fuzzy Token Set Ratio {a}")
+if __name__ =="__main__":
+    getMatches("Jhandi Chountra", city)
 
-choices = ["Atlanta Falcons", "New York Jets", "New York Giants", "Dallas Cowboys"]
-b = process.extract("new york jets", choices, limit=2)
-print(f"Process Extract: {b}")
+    # x = fuzz.ratio("Catherine M Gitau","Catherine Gitau")
+    # print(f"Fuzzy Ratio {x}")
+    # y = fuzz.partial_ratio("Catherine M. Gitau","Catherine Gitau")
+    # print(f"Fuzzy Partial Ratio {y}")
 
-c = process.extractOne("cowboys", choices)
-print(f"Process Extract One: {c}")
+    # z = fuzz.token_sort_ratio("Catherine Gitau M.", "Gitau Catherine")
+    # print(f"Fuzzy Token Sort Ratio {z}")
 
-d = process.extractOne("System of a down - Hypnotize - Heroin", songs)
+    # a = fuzz.token_set_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
+    # print(f"Fuzzy Token Set Ratio {a}")
+
+    # choices = ["Atlanta Falcons", "New York Jets", "New York Giants", "Dallas Cowboys"]
+    # b = process.extract("new york jets", choices, limit=2)
+    # print(f"Process Extract: {b}")
+
+    # c = process.extractOne("cowboys", choices)
+    # print(f"Process Extract One: {c}")
 
