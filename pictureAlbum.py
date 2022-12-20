5607b2a3ac800fbb7d6ac7007b5b2a6acc7825df
atifsalam78 <atif.salam@gmail.com>
2022-12-20 19:45:34 +0500

    Update layout

--- pictureAlbum.py
+++ pictureAlbum.py
@@ -3,48 +3,45 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 import os
 
-root = Tk()
-root.geometry("500x600")
-
-
 def loadImage():        
         for z in listAll:
-            yield z
-
-x = loadImage()      
-def nextButton():
-    try:
-        global img
-        global nextValue
-        nextValue = next(x)
-        img = ImageTk.PhotoImage(Image.open(nextValue))
-        # frame1 = Frame(master=root).grid(row=0, column=10) 
-        label2 = Label(image=img).grid(row=5, column=50)
-    except StopIteration:
-        messagebox.showwarning("showinfo", "File Ends")       
+            yield z    
         
 
 if __name__=="__main__":
 
-    parentPath = "C:/"
+    parentPath = "/home/atif/"
     currentWorkingDirectory = os.getcwd()   
-    changeDirectory = "officeup/sprites"
+    changeDirectory = "PycharmProjects/pythonProject/Tkinter Project/Images/"
     path = os.path.join(parentPath, changeDirectory)
     changePath = os.chdir(path) 
     currentWorkingDirectory = os.getcwd()
     listAll = os.listdir(currentWorkingDirectory)
-    # print(listAll)
+   
+    root = Tk()
+    root.geometry("900x600")
+    root.title("My Picture Album")
 
     var = StringVar()
-    var.set(" ".join("\n{0}".format(f) for f in listAll))  
-    listFrame = Frame(master=root).grid(row=0, column=1)     
-    label = Label(master=listFrame, text="List of files and directories from current path", relief=SUNKEN,textvariable=var).grid(row=0, column=1)
-    # frame2 = Frame(master=root).grid(row=0, column=25)
-    # label = Label(master=frame2, text="Image", font=("Times 16 bold"),relief=SUNKEN).grid()
-    
-    frame3 = Frame(master=root).grid(row=2, column=5)     
-    bt = Button(text="Next >>", command=nextButton).grid()
-
-
+    var.set(" ".join("\n{0}".format(f) for f in listAll))    
+    # frameLabelTitle = Frame(master=root).grid(padx=50, column=50)
+    labelTitle = Label(text="My Picture Album", font=("Times 16 bold")).grid(column=0)
+    # listFrame = Frame(master=root).grid(column=70)     
+    label = Label(text="List of files and directories from current path", relief=SUNKEN,textvariable=var).grid(row=1)
+     
+    x = loadImage()      
+    def nextButton():    
+        try:
+            global img
+            global nextValue
+            nextValue = next(x)
+            img = ImageTk.PhotoImage(Image.open(nextValue))               
+            label2 = Label(image=img, width=500, height=350,relief=SUNKEN).grid(padx=100, row=1, column=50)    
+        except StopIteration:
+            messagebox.showwarning("showinfo", "File Ends")
+            
+    # frame3 = Frame(master=root, pady=10).grid()    
+    btNext = Button(text="Next >>", width=10, height=1, font=("Helvetica 10 bold"),bg="skyblue", command=nextButton).grid(row=2, pady=5)
+    btExit = Button(text="Exit", width=10, height=1, font=("Helvetica 10 bold"),bg="pink", command=root.destroy).grid(row=3, pady=5)
 
     root.mainloop()
